f59f5631b071ad3dfad48863930644960da8275c
eh <eh@MacBook-Pro-cua-eh.local>
2023-04-17 07:31:04 +0700

    add api + testing

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -1,41 +1,41 @@
-# class BaseService
-#   attr_reader :errors
-#
-#   def initialize(*_args)
-#     @errors = []
-#   end
-#
-#   def self.call(*args)
-#     service = new(*args)
-#     Rails.logger.info "capture call"
-#     service.call
-#     service
-#   end
-#
-#   def success?
-#     errors.empty?
-#   end
-#
-#   def error?
-#     !success?
-#   end
-#
-#   def first_error
-#     errors.first
-#   end
-#
-#   private
-#
-#   def add_error(error)
-#     @errors ||= []
-#
-#     case error
-#     when StandardError, ActiveModel::Error
-#       errors << error
-#     when Array, ActiveModel::Errors
-#       error.each { |e| add_error(e) }
-#     else
-#       errors << StandardError.new(error)
-#     end
-#   end
-# end
+class BaseService
+  attr_reader :errors
+
+  def initialize(*_args)
+    @errors = []
+  end
+
+  def self.call(*args)
+    service = new(*args)
+    Rails.logger.info "capture call"
+    service.call
+    service
+  end
+
+  def success?
+    errors.empty?
+  end
+
+  def error?
+    !success?
+  end
+
+  def first_error
+    errors.first
+  end
+
+  private
+
+  def add_error(error)
+    @errors ||= []
+
+    case error
+    when StandardError, ActiveModel::Error
+      errors << error
+    when Array, ActiveModel::Errors
+      error.each { |e| add_error(e) }
+    else
+      errors << StandardError.new(error)
+    end
+  end
+end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
-# FactoryBot.define do
-#   factory :user do
-#     email { Faker::Internet.email }
-#     username { Faker::Internet.username(specifier: 6..8) }
-#     password { Faker::Internet.password(min_length: 10) }
-#   end
-# end
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    username { Faker::Internet.username(specifier: 6..8) }
+    password { Faker::Internet.password(min_length: 10) }
+  end
+end
